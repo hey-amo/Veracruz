a3f5cd2e68357cb448f8fb0b476ed3fe28a0080e
AD <silentflute.ad@gmail.com>
2025-10-30 18:47:44 +0000

    Adding hud cell view

---
+++ Veracruz/Veracruz/Game/Views/HUDCellView.swift
@@ -0,0 +1,18 @@
+//
+//  HUDCellView.swift
+//  Veracruz
+//
+//  Created by Amarjit on 30/10/2025.
+//
+
+import SwiftUI
+
+struct HUDCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HUDCellView()
+}
